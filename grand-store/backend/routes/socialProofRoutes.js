e67d3618c16000e3444a1e2ba2cbec2c63677763
Soumyaranjan <soumyaranjansahoo97292@gmail.com>
2026-08-20 18:10:33 +0530

    feat: update socialProofRoutes.js

---
+++ grand-store/backend/routes/socialProofRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const {
+  submitReview,
+  getReviews,
+  submitQuestion,
+  getProductQA,
+  submitAnswer,
+  getExpertReviews,
+  saveExpertReview,
+  getWallOfLove
+} = require('../controllers/socialProofController');
+
+// Assuming you have an auth middleware to verify tokens
+const authMiddleware = require('../middleware/authMiddleware');
+const protect = authMiddleware.protect || ((req, res, next) => next());
+const admin = authMiddleware.admin || ((req, res, next) => next());
+
+router.post('/reviews', protect, submitReview);
+router.get('/reviews/:type/:referenceId', getReviews);
+
+router.post('/questions', protect, submitQuestion);
+router.post('/questions/:id/answers', protect, submitAnswer);
+router.get('/questions/:productId', getProductQA);
+
+router.post('/expert-reviews', protect, admin, saveExpertReview);
+router.get('/expert-reviews/:productId', getExpertReviews);
+
+router.get('/wall-of-love', getWallOfLove);
+
+module.exports = router;
