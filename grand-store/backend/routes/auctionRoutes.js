77eb7c9a081a8f21941264b3254457a1459b468a
Soumyaranjan <soumyaranjansahoo97292@gmail.com>
2026-08-25 14:02:37 +0530

    Update backend/routes/auctionRoutes.js

--- grand-store/backend/routes/auctionRoutes.js
+++ grand-store/backend/routes/auctionRoutes.js
@@ -14,15 +14,7 @@ router.post('/:id/pay', protect, auctionController.payAuction);
 router.get('/user/dashboard', protect, auctionController.getUserBids);
 
 const multer = require('multer');
-const path = require('path');
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename(req, file, cb) {
-    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
-  }
-});
+const { storage } = require('../config/cloudinary');
 const upload = multer({ storage });
 
 // Vendor
